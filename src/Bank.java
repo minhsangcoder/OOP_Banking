c5e12ae44a8dee42f6422d30e38a33e7feb01925
minhsangcoder <minhsangcoder@gmail.com>
2024-12-05 10:53:30 +0700

    first commit

---
+++ src/Bank.java
@@ -0,0 +1,21 @@
+package src;
+
+public class Bank {
+    List<BankAccount>accounts;
+}
+
+public addAccount (BankAccount account) {
+
+}
+
+public findAccount (String accountNumber) {
+
+}
+
+public removeAccount (String accountNumber) {
+
+}
+
+public void listAccounts() {
+
+}
---
+++ src/BankAccount.java
@@ -0,0 +1,24 @@
+package src;
+
+public class BankAccount {
+    private String accountNumber;
+    private String accounHolderName;
+    private double balance;
+    private String accountType;
+}
+
+public void deposit (double amount) {
+
+}
+
+public void withdraw (double amount) {
+
+}
+
+public void checkBalance () {
+
+}
+
+public void getAccountInfo () {
+
+}
